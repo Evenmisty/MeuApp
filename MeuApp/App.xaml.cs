ae1039d607d82005e2b1f88d9ef622be925b725c
palom <palomamribeiro27@gmail.com>
2025-03-09 22:44:27 -0300

    Criada a tela de inserção do produto e incluídos alguns botões.

--- MeuApp/App.xaml.cs
+++ MeuApp/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace MeuApp
+﻿using MeuApp.Helpers;
+
+namespace MeuApp
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _Db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get 
+            {
+                if (_Db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3"); 
+                    
+                    _Db = new SQLiteDatabaseHelper(".... db3"); 
+                }
+
+
+                return _Db;
+            }
+        }
         public App()
         {
               InitializeComponent();
